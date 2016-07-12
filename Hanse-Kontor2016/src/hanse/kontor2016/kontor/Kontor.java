d47111f49bbb807e3da538574147e21c7680d943
sbastian <sbastian@kil-m-23.stl.htwsaar.de>
2016-07-12 18:05:35 +0200

    Kontor angefangen

--- Hanse-Kontor2016/src/hanse/kontor2016/kontor/Kontor.java
+++ Hanse-Kontor2016/src/hanse/kontor2016/kontor/Kontor.java
@@ -1,8 +1,17 @@
 package hanse.kontor2016.kontor;
 
 import hanse.kontor2016.Definitionen;
+import hanse.kontor2016.kaufmann.Kaufmann;
 
 public class Kontor {
 	private Ware lokaleware;
 	Lager lager = new Lager(Definitionen.lagerKapazitaetKontor);
+
+	public void wareImportieren(Kaufmann kaufmann, Ware ware, int menge) {
+		if (ware == lokaleware) { //Ware wird nur in Geld umgewandelt, da sie nicht gelagert wird
+			kaufmann.zahleBetragEin(menge*Definitionen.preisRegionalesGut);
+		} else {
+			//TODO ware einlagern, geld an Kaufmann zahlen, Warenpreis ggf. anpassen.
+		}
+	}
 }
