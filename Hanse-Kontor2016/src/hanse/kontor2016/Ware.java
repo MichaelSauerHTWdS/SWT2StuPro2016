187ff7da3cc30be1a4b2712963dffb0b0a0dba9b
deMave <mst2907@aol.com>
2016-07-18 08:34:00 +0200

    Add files via upload

---
+++ Hanse-Kontor2016/src/hanse/kontor2016/Ware.java
@@ -0,0 +1,12 @@
+package hanse.kontor2016;
+
+public class Ware {
+	public String name="";
+	public int menge=0;
+	
+	public Ware(String name, int menge) {
+		this.name = name;
+		this.menge = menge;
+	}
+
+}
